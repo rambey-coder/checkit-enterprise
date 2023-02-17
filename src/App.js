63e14193db3ec7ea98818e3a9d16be14f756d2a9
rambey-coder <olayiwolaagboolaakaanji@gmail>
2023-02-17 19:42:49 +0100

    feat: added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Login from "./pages/signIn/Login"
-import SignUp from "./pages/signUp/SignUp"
+import "./App.css";
+import Login from "./pages/signIn/Login";
+import SignUp from "./pages/signUp/SignUp";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <SignUp />
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
