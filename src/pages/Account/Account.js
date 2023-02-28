738f2ca49d9ec3ef90dd17a9a054a1136b95ee0f
rambey-coder <olayiwolaagboolaakaanji@gmail>
2023-02-28 22:28:14 +0100

    refactor dashboard

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -4,7 +4,8 @@ import { useAppContext } from "../../context/Context";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import styles from "./Account.module.css";
 
-import user from "./assets/user.svg";
+// import user from "./assets/user.svg";
+import add from "./assets/add.svg"
 
 const Account = () => {
   const { handleLogout } = useAppContext();
@@ -13,33 +14,96 @@ const Account = () => {
   return (
     <div className={styles.container}>
       <Sidebar />
-      <div className={styles.container_content}>
-        <div className={styles.user_details}>
-          <div className={styles.user_bg}>
-            <img src={user} alt="" />
+      <div className={styles.general}>
+        <div className={styles.container_content}>
+          {/* <div className={styles.user_details}>
+            <div className={styles.user_bg}>
+              <img src={user} alt="user" />
+            </div>
+
+            <div className={styles.details}>
+              <div>
+                <p>
+                  Username: &nbsp;
+                  <span>{currentUser?.username}</span>
+                </p>
+                <p></p>
+              </div>
+
+              <div>
+                <p>
+                  Email Address: &nbsp;
+                  <span>{currentUser?.email}</span>
+                </p>
+              </div>
+
+              <div>
+                <p>
+                  User ID: &nbsp;
+                  <span>{currentUser?.id}</span>
+                </p>
+              </div>
+            </div>
+          </div> */}
+
+          <div className={styles.user_details}>
+            <div className={styles.user_bg}>
+              <img src={add} alt="add" />
+            </div>
+
+            <div className={styles.details}>
+              <div>
+                <p>
+                  Delivery Address: &nbsp;
+                  <span>{currentUser?.username}</span>
+                </p>
+                <p></p>
+              </div>
+
+              <div>
+                <p>
+                  China Address: &nbsp;
+                  <span>{currentUser?.id}</span>
+                </p>
+              </div>
+
+              <div>
+                <p>
+                  User ID: &nbsp;
+                  <span>{currentUser?.id}</span>
+                </p>
+              </div>
+
+            </div>
           </div>
 
-          <div className={styles.details}>
-            <div>
-              <p>
-                Username: &nbsp;
-                <span>{currentUser?.username}</span>
-              </p>
-              <p></p>
+          <div className={styles.user_details}>
+            <div className={styles.user_bg}>
+              <img src={add} alt="add" />
             </div>
 
-            <div>
-              <p>
-                Email Address: &nbsp;
-                <span>{currentUser?.email}</span>
-              </p>
-            </div>
+            <div className={styles.details}>
+              <div>
+                <p>
+                  Exchange rate: &nbsp;
+                  <span>{currentUser?.username}</span>
+                </p>
+                <p></p>
+              </div>
 
-            <div>
-              <p>
-                User ID: &nbsp;
-                <span>{currentUser?.id}</span>
-              </p>
+              <div>
+                <p>
+                  China Address: &nbsp;
+                  <span>{currentUser?.id}</span>
+                </p>
+              </div>
+
+              <div>
+                <p>
+                  Phone number: &nbsp;
+                  <span>{currentUser?.id}</span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
