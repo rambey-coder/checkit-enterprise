b620ca6bf7d233f26df3fb08988009daffda25f3
rambey-coder <olayiwolaagboolaakaanji@gmail>
2023-03-05 12:03:10 +0100

    style: style create order

--- src/pages/CreateOrder/CreateOrder.js
+++ src/pages/CreateOrder/CreateOrder.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import "./CreateOrder.module.css"
+import styles from "./CreateOrder.module.css"
 
 import Sidebar from '../../components/Sidebar/Sidebar'
 
+
 const CreateOrder = () => {
   return (
-      <div>
-          <Sidebar />
-          
+    <div className={styles.container}>
+      <Sidebar />
+      <div className={styles.general}>
+        <div className={styles.content_container}>
+        <div className={styles.content}>
+          <h3>Create an Order</h3>
+          <form>
+            <div>
+              <label htmlFor="link">Order Link</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="add">Delivery Address</label>
+              <input type="text" />
+            </div>
+            <button>Create Order</button>
+          </form>
+        </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CreateOrder
