119f62d76b588776620a22118d5b96f8903be24d
rambey-coder <olayiwolaagboolaakaanji@gmail>
2023-03-05 19:02:04 +0100

    refactor: error handler

--- src/ToolKit/ApiRequest/Errorhandler.js
+++ src/ToolKit/ApiRequest/Errorhandler.js
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 const Errorhandler = (error) => {
   console.log(error);
-  if (error.code === "ERR_NETWORK") {
+  if (error.message === "Network Error") {
     toast.error("Network Error");
   } else if (error.code === "ERR_BAD_REQUEST") {
     toast.error("Something Went wrong");
