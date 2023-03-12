26907bde4596a40c599eb314f376fa2cd11a91f3
rambey-coder <olayiwolaagboolaakaanji@gmail>
2023-03-12 11:53:26 +0100

    style: admin order detail

--- src/pages/TrackOrder/Admin/AdminOrderDetails.js
+++ src/pages/TrackOrder/Admin/AdminOrderDetails.js
@@ -1,12 +1,14 @@
 import { React, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import styles from "./Admin.module.css";
 
 import { getOrderDetail } from "../../../ToolKit/Features/Admin/Service";
 import { ShortenTextLength } from "../../../components/Functions/ShortTextLength";
 
 import trash from "../assets/trash.svg";
 import edit from "../assets/edit.svg";
+import date from "./assets/calendar.svg"
 
 const AdminOrderDetails = () => {
   const { id } = useParams();
@@ -18,11 +20,47 @@ const AdminOrderDetails = () => {
     dispatch(getOrderDetail(id));
   }, [dispatch, id]);
   return (
-    <div>
+    <div className={styles.container}>
       <div>
-        <h2>Order Details</h2>
+        <div className={styles.head}>
+          <h2>Order Details</h2>
 
-        {/* <div>
+          <div>
+            <img src={date} alt="date" />
+            <p>{orderDetail?.created}</p>
+          </div>
+        </div>
+        <div className={styles.details}>
+          <div>
+            <p className={styles.name_id}>Order Link:</p>
+            <p>{ShortenTextLength(orderDetail?.links)}</p>
+          </div>
+          <div>
+            <p className={styles.name_id}>Order ID:</p>
+            <p>{orderDetail?.id}</p>
+          </div>
+          <div>
+            <p className={styles.name_id}>Order charge:</p>
+            <p>{orderDetail?.chargeRequest}</p>
+          </div>
+          <div>
+            <p className={styles.name_id}>Delivery Address:</p>
+            <p>{ShortenTextLength(orderDetail?.deliveryAddress)}</p>
+          </div>
+          <div>
+            <p className={styles.name_id}>Order Price:</p>
+            <p>{orderDetail?.orderPrice}</p>
+          </div>
+          <div>
+            <p className={styles.name_id}>Order status:</p>
+            <p>{orderDetail?.orderStatus}</p>
+          </div>
+          <div>
+            <p className={styles.name_id}>Payment status:</p>
+            <p>{orderDetail?.pay}</p>
+          </div>
+        </div>
+        <div>
           <div>
             <img src={trash} alt="trash" />
             <p>Delete</p>
@@ -31,37 +69,6 @@ const AdminOrderDetails = () => {
             <img src={edit} alt="trash" />
             <p>Edit</p>
           </div>
-        </div> */}
-        <p>{orderDetail?.created}</p>
-      </div>
-      <div>
-        <div>
-          <p>Order Link</p>
-          <p>{ShortenTextLength(orderDetail?.links)}</p>
-        </div>
-        <div>
-          <p>Order ID</p>
-          <p>{orderDetail?.id}</p>
-        </div>
-        <div>
-          <p>Order charge</p>
-          <p>{orderDetail?.chargeRequest}</p>
-        </div>
-        <div>
-          <p>Delivery Address</p>
-          <p>{ShortenTextLength(orderDetail?.deliveryAddress)}</p>
-        </div>
-        <div>
-          <p>Order Price</p>
-          <p>{orderDetail?.orderPrice}</p>
-        </div>
-        <div>
-          <p>Order status</p>
-          <p>{orderDetail?.orderStatus}</p>
-        </div>
-        <div>
-          <p>Payment status</p>
-          <p>{orderDetail?.pay}</p>
         </div>
       </div>
     </div>
